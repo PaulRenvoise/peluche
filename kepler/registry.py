bc4d2895718c1d7b3b316a3b38c5de7283763303
Paul Renvoisé <renvoisepaul@gmail.com>
2019-10-21 00:09:09 +0200

    doc(registry) add doc as TODO

--- kepler/registry.py
+++ kepler/registry.py
@@ -2,10 +2,19 @@ from collections import defaultdict
 
 
 class Registry():
+    """
+    TODO
+    """
     def __init__(self):
+        """
+        TODO
+        """
         self._registry = defaultdict(list)
 
     def register(self, registree):
+        """
+        TODO
+        """
         self._registry[registree.name].append(registree)
 
     @property
