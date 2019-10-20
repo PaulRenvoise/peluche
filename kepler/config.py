932b07331e346860f465c6df517b763a652e2767
Paul Renvoisé <renvoisepaul@gmail.com>
2019-10-21 00:06:34 +0200

    doc(config) add doc as TODO

--- kepler/config.py
+++ kepler/config.py
@@ -2,9 +2,13 @@ from configparser import ConfigParser
 
 
 class Config():
+    """
+    TODO
+    """
     def __init__(self, config_file):
+        """
+        TODO
+        """
         parser = ConfigParser()
 
         parser.read(config_file)
-
-
