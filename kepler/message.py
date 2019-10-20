8d2c83f228b906e0f378a3b9d0b3cf86f4b0a79e
Paul Renvoisé <renvoisepaul@gmail.com>
2019-10-21 00:06:51 +0200

    doc(message) add doc as TODO

--- kepler/message.py
+++ kepler/message.py
@@ -1,11 +1,17 @@
 from .store import Store as Storable
 
 class Message(Storable):
+    """
+    TODO
+    """
     TYPES = {'info', 'convention', 'suggestion', 'refactor', 'warning', 'error', 'fatal'}
     TYPES_SHORT_TO_LONG = {t[0].upper(): t for t in TYPES}
     TYPES_LONG_TO_SHORT = {t: t[0].upper() for t in TYPES}
 
     def __init__(self, error_id, content, node=None):
+        """
+        TODO
+        """
         line = node.absolute_top_line
         column = node.absolute_left_column
 
