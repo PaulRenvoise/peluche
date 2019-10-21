0087167bf53d787267509282dfc4af5a65ffaf11
Paul Renvoisé <renvoisepaul@gmail.com>
2019-10-21 23:38:10 +0200

    refactor(source) make fst and ast properties, compute sha1 of the file

--- kepler/source.py
+++ kepler/source.py
@@ -1,26 +1,57 @@
+import os
+import hashlib
+
 import baron
 from redbaron import RedBaron
 
 
 class Source:
-    def __init__(self, filename):
-        self.filename = filename
+    """
+    TODO
+    """
+    def __init__(self, filepath):
+        """
+        TODO
+        """
+        self.filepath = filepath
+        self.filename = os.path.split(filepath)[1]
+        self.extension = os.path.splitext(self.filename)[1]
 
-        self.raw = open(filename, 'r').read()
+        self.raw = open(filepath, 'r').read()
+        self.lines = self.raw.splitlines()
 
-        self.ast, self.fst = self._parse(self.raw)
+        self.sha1 = hashlib.sha1(self.raw.encode('utf-8')).hexdigest()
 
         # Sets the deactivated checkers per line
-        self.lines_count = len(self.raw.splitlines())
-        self.lines = [set() for _ in range(self.lines_count)]
+        # self.lines_count = len(self.lines)
+        # self.lines = [set() for _ in range(self.lines_count)]
 
-        for comment in self.ast.find_all('comment'):
-            if comment.value.startswith('# kepler:disable '):
-                self._disable_checkers_with_comment(comment)
-            elif comment.value.startswith('# kepler:enable '):
-                self._enable_checkers_with_comment(comment)
-            else:
-                continue
+        # for comment in self.ast.find_all('comment'):
+        #     if comment.value.startswith('# kepler:disable '):
+        #         self._disable_checkers_with_comment(comment)
+        #     elif comment.value.startswith('# kepler:enable '):
+        #         self._enable_checkers_with_comment(comment)
+        #     else:
+        #         continue
+
+    @property
+    def ast(self):
+        try:
+            return self._ast
+        except AttributeError:
+            self._ast = self._parse(self.raw)
+
+            return self._ast
+
+    @property
+    def fst(self):
+        try:
+            return self._fst
+        except AttributeError:
+            # Calls `.ast()` that will call `._parse()` if `_ast` is not set
+            self._fst = self.ast.fst()
+
+            return self._fst
 
     def _parse(self, raw):
         try:
@@ -29,7 +60,7 @@ class Source:
             # TODO: log something? What's the wanted behavior here?
             raise error
 
-        return [ast, ast.fst()]
+        return ast
 
     def _disable_checkers_with_comment(comment):
         checkers = set(comment.value[17:].split(', '))
@@ -52,7 +83,7 @@ class Source:
             self.lines[position.line - 1] -= checkers
 
     def __repr__(self):
-        return self.filename
+        return self.filepath
 
     def __str__(self):
-        return self.filename
+        return self.filepath
