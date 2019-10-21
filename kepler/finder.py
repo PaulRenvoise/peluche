be3225030ad12ecb36166668a5f80d1905743d46
Paul Renvoisé <renvoisepaul@gmail.com>
2019-10-21 23:40:29 +0200

    refactor(finder) implement a deduplication that actually works, remove comments from protected methods and add them to the public method

--- kepler/finder.py
+++ kepler/finder.py
@@ -1,82 +1,62 @@
 import os
 import glob
-import time
+import logging
 from itertools import combinations
+
+from .tracer import tracer
 
 
 class Finder():
+    """
+    TODO
+    """
     def find_files(self, paths):
         """
-        Orchestrates the normalization and near-deduplication of paths,
-        as well as the finding and filtering of files from a list of paths
+        Orchestrates the search, normalization, deduplication, and filtering of files from a list of paths
+
+        Handles environment variables (such as $HOME, $NAME, or $WHATEVER), '~', and symlinks
+
+        Args:
+            - paths (list<str>): the paths in which to find the files
+
+        Returns:
+            - list(<str>): the list of files found
         """
-        before = time.time()
+        with tracer('Finding files'):
+            paths = self._normalize_paths(paths)
 
-        paths = self._normalize_paths(paths)
-        print(paths)
+            paths = self._cleanup_paths(paths)
 
-        paths = self._cleanup_paths(paths)
-        print(paths)
+            files = self._expand_paths_to_files(paths)
 
-        files = self._expand_paths_to_files(paths)
-        print(len(files))
+            files = self._filter_files(files)
 
-        print(f"{time.time() - before}")
-        return []
+            logging.debug("  Found %i matching files!", len(files))
 
-        files = self._filter_files(files)
 
         return files
 
     def _normalize_paths(self, paths):
-        """
-        Normalizes paths to return their absolute fully expanded form
-
-        - `.expandvars()` handles environment variables (such as $HOME, $NAME, or $WHATEVER)
-        - `.expanduser()` handles '~'
-        - `.realpath()` resolves simlink AND the absolute path (just like `.abspath()`)
-
-        Args:
-            - paths (list<str>) : the paths to normalize
-
-        Returns:
-            - list<str> : the absolute expanded paths
-        """
         return [os.path.realpath(os.path.expanduser(os.path.expandvars(path))) for path in paths]
 
     def _cleanup_paths(self, paths):
-        """
-        Removes redundant paths, such as duplicates, or paths that are nested in at least another path
-
-        Args:
-            - paths (list<str>) : the paths to cleanup
-
-        Returns:
-            - list<str> : the cleaned up paths
-        """
         sorted_paths = sorted(paths, key=len)
-        non_nested_paths = set([sorted_paths[0]])  # first path is the shortest so it MUST be processed
 
-        for index, lhs in enumerate(sorted_paths):
+        non_nested_paths = set(sorted_paths)
+
+        for index, lhs in enumerate(sorted_paths, 1):
             if os.path.isfile(lhs):
                 continue
 
             for rhs in sorted_paths[index:]:
-                if not rhs.startswith(lhs):
-                    non_nested_paths.update([rhs])
+                if rhs.startswith(lhs) and rhs in non_nested_paths:
+                    non_nested_paths.remove(rhs)
+
+        logging.debug("  Discarded %i nested paths", len(paths) - len(non_nested_paths))
 
         return non_nested_paths
 
     def _expand_paths_to_files(self, paths):
-        """
-        Recursively extracts the files from the paths
-
-        Args:
-            - paths (list<str>) : the paths to extract the files from
-
-        Returns:
-            - list<str> : the files extracted from the paths
-        """
         expanded_files = []
 
         for path in paths:
@@ -99,13 +79,4 @@ class Finder():
         return expanded_files
 
     def _filter_files(self, files):
-        """
-        Filters unwanted files (following a config) from a given set of files
-
-        Args:
-            - files (list<str>) : the list of files to reduce
-
-        Returns:
-            - list<str> : the filtered list of files
-        """
         return files
